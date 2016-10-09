0320c1c32fd20fc891078db647dfecce1abcc14a
vndbjlqx <iamlqx@126.com>
2016-10-09 19:04:21 +0800

    change http to https in data.module for AngularJSM4Assignment

--- AngularJSM4Assignment/src/menulist/data.module.js
+++ AngularJSM4Assignment/src/menulist/data.module.js
@@ -2,6 +2,6 @@
 'use strict';
 
 angular.module('data', [])
-.constant('ApiBasePath', 'http://davids-restaurant.herokuapp.com');
+.constant('ApiBasePath', 'https://davids-restaurant.herokuapp.com');
 
 })();
