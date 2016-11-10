46b814ba3c3684afba05dacdf3b3400f0639970b
vndbjlqx <iamlqx@126.com>
2016-11-10 09:02:09 +0800

    some code for signup page

--- AngularJSM5Assignment/src/public/public.routes.js
+++ AngularJSM5Assignment/src/public/public.routes.js
@@ -40,6 +40,12 @@ function routeConfig ($stateProvider) {
           return MenuService.getMenuItems($stateParams.category);
         }]
       }
+    })
+    .state('public.signup', {
+      url:'/signup',
+      templateUrl:'src/public/signup/signup.html',
+      controller:'RegistrationController',
+      controllerAs:'reg'
     });
 }
 })();
